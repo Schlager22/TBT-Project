b5afa483a9b648b578b270da51b38e1b0eb1a24f
Andrew Wolschlag <apwolsch@ncsu.edu>
2023-06-10 09:04:10 -0400

    first commit

---
+++ TBT v0.1.py
@@ -0,0 +1,74 @@
+# Welcome to TBT (Text Based TFT)
+
+
+# Import Packages
+#import pandas as pd
+#import numpy as np
+
+# Define Objects
+all_champs = [
+'Aatrox',
+'Ahri',
+'Akshan',
+'Aphelios',
+'Ashe',
+'Azir',
+'Bel_Veth',
+'Cassiopeia'
+'Cho_Gath',
+'Darius',
+'Ekko',
+'Galio',
+'Garen',
+'Gwen',
+'Heimerdinger',
+'Irelia',
+'Jarvan IV',
+'Jayce',
+'Jhin',
+'Jinx',
+'KSante',
+'Kai_Sa',
+'Kalista',
+'Karma',
+'Kassadin',
+'Katarina',
+'Kayle',
+'Kled',
+'Lissandra',
+'Lux',
+'Malzahar',
+'Maokai',
+'Nasus',
+'Orianna',
+'Poppy',
+'Rek_Sai',
+'Renekton',
+'Ryze',
+'Samira',
+'Sejuani',
+'Senna',
+'Sett',
+'Shen',
+'Sion',
+'Sona',
+'Soraka',
+'Swain',
+'Taliyah',
+'Taric',
+'Teemo',
+'Tristana',
+'Urgot',
+'Vel_Koz',
+'Vi',
+'Viego',
+'Warwick',
+'Yasuo',
+'Zed',
+'Zeri']
+
+# Define Rules
+
+# Game Execution
+all_champs.count()
+
